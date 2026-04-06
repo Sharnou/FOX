0e5d1a088482fa8ab95e40c5bed942c2f7ba59a3
FOX <Ahmed_sharnou@yahoo.com>
2026-04-06 21:42:19 +0200

    fix: remove backtick template literals from form.jsx

--- frontend/components/ui/form.jsx
+++ frontend/components/ui/form.jsx
@@ -38,9 +38,9 @@ const useFormField = () => {
   return {
     id,
     name: fieldContext.name,
-    formItemId: `${id}-form-item`,
-    formDescriptionId: `${id}-form-item-description`,
-    formMessageId: `${id}-form-item-message`,
+    formItemId: id + '-form-item',
+    formDescriptionId: id + '-form-item-description',
+    formMessageId: id + '-form-item-message',
     ...fieldState,
   }
 }
@@ -80,8 +80,8 @@ const FormControl = React.forwardRef(({ ...props }, ref) => {
       id={formItemId}
       aria-describedby={
         !error
-          ? `${formDescriptionId}`
-          : `${formDescriptionId} ${formMessageId}`
+          ? formDescriptionId
+          : formDescriptionId + ' ' + formMessageId
       }
       aria-invalid={!!error}
       {...props} />)
